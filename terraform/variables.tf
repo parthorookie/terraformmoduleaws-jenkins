4d45e9dedcf206046a7f32316ceceac8894342f8
Partho M <parthomazumdar999@gmail.com>
2025-10-23 20:18:31 +0530

    us east variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"  # Use US region instead of ap-southeast-1
+}
+
 variable "app_image" {
   description = "Docker image to run"
   type        = string
   default     = "ghcr.io/parthorookie/aws-waf-gitops:latest"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"  # Amazon Linux 2023 in us-east-1
 }
